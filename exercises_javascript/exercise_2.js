dfba9055c0dc04ad73ca0848e4194282d31e8e76
hpeteralmeida <masterff0684@gmail.com>
2026-05-15 09:31:45 -0300

    feat: add exercise 2

---
+++ exercises_javascript/exercise_2.js
@@ -0,0 +1,9 @@
+//funcao que recebe um nome e mostra uma saudacao
+
+function saudar(nome) {
+    return "Olá, " + nome + "!";
+}
+
+let nome = "Padawan";
+let saudacao = saudar(nome);
+console.log(saudacao);
